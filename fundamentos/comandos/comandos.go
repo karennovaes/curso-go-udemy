4db47bfad6ac6b095155ee486a030a77ce416517
Karen Novaes <karen-nd@outlook.com>
2023-05-23 15:12:27 -0300

    Explorando os Comandos do Terminal

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro programa em %s!\n", "Go")
+}
+
+/*
+Comandos utilizados:
+
+- go
+- go help get
+- go version
+- godoc -http=:6060 : documentação offline
+- go env : infos sobre como e onde está instalado
+- go vet arquivo.go : realiza algumas verificações no código
+- go run arquivo.go
+
+*/
